6dbe95766739b0b80936d9a2dfa6ce1f4d1124e4
YurNeim <mpatrashkov2002@outook.com>
2018-06-24 10:14:31 +0300

    dasda

---
+++ softuni/src/components/MainView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Toolbar from './Toolbar';
+
+class MainView extends Component {
+
+    render() {
+        return (
+            <div>
+                <Toolbar/>
+            </div>
+        );
+    }
+}
+
+export default MainView;
---
+++ softuni/src/components/Toolbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Toolbar extends Component {
+
+    render() {
+        return (
+            <div id="toolbar">
+                <span>Test</span>
+            </div>
+        );
+    }
+}
+
+export default Toolbar;
