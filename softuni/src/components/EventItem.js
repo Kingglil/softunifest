961c5ac73cb16a0066f352535a54981f913a4110
YurNeim <mpatrashkov2002@outook.com>
2018-06-24 13:59:35 +0300

    Rnf

--- softuni/src/components/EventItem.js
+++ softuni/src/components/EventItem.js
@@ -1,12 +1,18 @@
 import React, { Calendar } from 'react';
 import './eventitem.css';
 
-class EventItem extends Component {
+class EventItem extends React.Component {
     
     render() {
         return (
-            <div class="eventContainer">
-                
+            <div class="event-container">
+                <div class="event-date">
+                    <span>23</span><br/>
+                    <span>June</span>
+                </div>
+                <div class="event-name">
+                    <span>Indepen as fdfsddf</span>
+                </div>
             </div>
         );
     }
